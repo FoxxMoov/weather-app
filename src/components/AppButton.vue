88eb06d71b77ae5b467ea1cfcabd891dacdce56d
FoxxMoov <corentin.ravet@gmail.com>
2020-08-09 22:31:55 +0200

    :lipstick: (Adding button)

---
+++ src/components/AppButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <component :is="is" v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  computed: {
+    is() {
+      return this.$attrs.to ? "router-link" : "button";
+    },
+  },
+};
+</script>
